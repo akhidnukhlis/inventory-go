0030512eb3967e0cf20e5e0f20332d0dc3d244b5
Akhid Nukhlis <nukhlis@gmail.com>
2021-07-05 14:47:17 +0700

    add Gopkg.toml and Gopkg.lock

--- controllers/supplier.controller.go
+++ controllers/supplier.controller.go
@@ -2,19 +2,14 @@ package controllers
 
 import (
 	"fmt"
-	"github.com/akhidnukhlis/models"
-	"github.com/labstack/echo"
 	"net/http"
 	"time"
+
+	"github.com/akhidnukhlis/models"
+	"github.com/labstack/echo"
 )
 
 //var layoutFormat = "2006-01-02 15:04:05"
-
-func getToday(format string) (todayString string){
-	today := time.Now()
-	todayString = today.Format(format);
-	return
-}
 
 func FetchAllSupplier(c echo.Context) error {
 	result, err := models.FetchAllSupplier()
@@ -26,18 +21,15 @@ func FetchAllSupplier(c echo.Context) error {
 }
 
 func StoreSupplier(c echo.Context) error {
-	SupplierID 		:= time.Now()
-	SupplierName 	:= c.FormValue("supplierName")
+	SupplierID := time.Now()
+	SupplierName := c.FormValue("supplierName")
 
 	convSupplierID, err := fmt.Println(SupplierID.Format("2006-01-02 15:04:05"))
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
 
-	cSID, err := fmt.Println("SUP", convSupplierID)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
+	cSID := fmt.Sprint("SUP", convSupplierID)
 
 	result, err := models.StoreSupplier(string(cSID), SupplierName)
 	if err != nil {
